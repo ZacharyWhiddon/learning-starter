87d77549aa7090db02858a01fb6f904000027b2a
Zachary Whiddon <zach@elevator3.com>
2022-02-15 14:54:43 -0600

    Init

---
+++ Web/LearningStarter/Controllers/AuthenticationController.cs
@@ -0,0 +1,79 @@
+﻿using LearningStarter.Common;
+using LearningStarter.Entities;
+using LearningStarter.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningStarter.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] LoginDto dto)
+        {
+            var response = new Response();
+
+            var isLoggedIn = _authenticationService.Login(dto.UserName, dto.Password);
+
+            if (!isLoggedIn)
+            {
+                response.AddError(string.Empty, "Username or password is incorrect");
+                return BadRequest(response);
+            }
+
+            response.Data = true;
+
+            return Ok(response);
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            _authenticationService.Logout();
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("get-current-user")]
+        public IActionResult GetLoggedInUser()
+        {
+            var response = new Response();
+
+            var user = _authenticationService.GetLoggedInUser();
+
+            if (user == null)
+            {
+                response.AddError(string.Empty, "There was an issue getting the logged in user.");
+                return NotFound(response);
+            }
+
+            var userGetDto = new UserGetDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.Username
+            };
+
+            response.Data = userGetDto;
+
+            return Ok(response);
+        }
+    }
+
+    public class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Web/LearningStarter/Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using LearningStarter.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningStarter.Data
+{
+    public sealed class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(x => x.FirstName)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.LastName)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.Username)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.Password)
+                .IsRequired();
+
+        }
+    }
+}
