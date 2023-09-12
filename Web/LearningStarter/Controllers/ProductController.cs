03e563612ffa8c5eaa1a5b5db5c45ed116cf8dcc
Zachary Whiddon <zach@elevator3.com>
2023-09-12 16:13:07 -0500

    commit message

---
+++ Web/LearningStarter/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using LearningStarter.Common;
+using LearningStarter.Data;
+using LearningStarter.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace LearningStarter.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ProductCreateDto product) 
+        {
+            var response = new Response();
+
+            var productToCreate = new Product
+            {
+                Name = product.Name,
+                Description = product.Description,
+            };
+
+            _dataContext.Set<Product>().Add(productToCreate);
+            _dataContext.SaveChanges();
+
+            var productToReturn = new ProductGetDto
+            {
+                Id = productToCreate.Id,
+                Name = productToCreate.Name,
+                Description = productToCreate.Description,
+            };
+
+            response.Data = productToReturn;
+            return Created("", response);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var response = new Response();
+            var data = _dataContext.Set<Product>()
+                .Select(x => new ProductGetDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                })
+                .ToList();
+            response.Data = data;
+            return Ok(response);
+        }
+    }
+}
---
+++ Web/LearningStarter/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningStarter.Entities
+{
+    public class Product : ProductGetDto
+    {
+    }
+
+    public class ProductCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ProductGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+        }
+    }
+}
